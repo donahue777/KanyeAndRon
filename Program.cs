785cee7de3be50ad908c54fc9ff18151c31a6dc0
Thomas Donahue <donahue777@yahoo.com>
2024-04-08 00:19:13 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Runtime.InteropServices.JavaScript;
+//using System.Net.Http;
+
+namespace KanyeAndRon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuoteGenerator.KanyeQuote();
+
+            QuoteGenerator.RonQuote();
+
+            QuoteGenerator.KanyeQuote();
+
+            QuoteGenerator.RonQuote();
+
+            QuoteGenerator.KanyeQuote();
+
+            QuoteGenerator.RonQuote();
+
+            QuoteGenerator.KanyeQuote();
+
+            QuoteGenerator.RonQuote();
+
+            QuoteGenerator.KanyeQuote();
+
+            QuoteGenerator.RonQuote();
+        }
+    }
+}
---
+++ QuoteGenerator.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanyeAndRon
+{
+    public class QuoteGenerator
+    {
+        public static void KanyeQuote()
+        {
+            HttpClient client = new HttpClient();
+
+            var kanyeURL = "https://api.kanye.rest";
+
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            Console.WriteLine($"Kanye: {kanyeQuote}");
+        }
+        public static void RonQuote()
+        {
+            HttpClient client = new HttpClient();
+
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronResponse = client.GetStringAsync(ronURL).Result;
+
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            Console.WriteLine("-------------------------------------------");
+
+            Console.WriteLine($"Ron: {ronQuote}");
+
+            Console.WriteLine("-------------------------------------------");
+        }
+    }
+}
